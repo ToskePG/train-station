2d62ea90d7e8602abf47d27421fa5f272459e152
Moderator <danilo.tosic.pmf@gmail.com>
2023-01-30 03:25:41 +0100

    creates funcitons for db connection

--- src/main/DatabaseConnection.java
+++ src/main/DatabaseConnection.java
@@ -1,5 +1,41 @@
 package main;
 
-public class DatabaseConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DatabaseConnection {
+	
+	private String url;
+	private String user;
+	private String pass;
+	
+	public DatabaseConnection(String url, String user, String pass) {
+		this.url = url;
+		this.user = user;
+		this.pass = pass;
+	}
+	
+	public Connection open() {
+		try {
+			return DriverManager.getConnection(url, user, pass);
+		}catch(SQLException e) {
+			System.out.println("Greska u konekciji sa bazom! ");
+			return null;
+		}
+	}
+	
+	public boolean close(Connection conn) {
+		try {
+			conn.close();
+			return true;
+		}catch(SQLException e) {
+			System.out.println("Greska. Nemoguce je zatvoriti konekciju ");
+			return false;
+		}
+	}
+	
+	
 }
